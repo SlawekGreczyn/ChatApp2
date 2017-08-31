fc4f2818da8eb0fea7dffa15569c0f75c88b4bfe
Slawomir Greczyn <slawomir.greczyn@whallalabs.com>
2017-08-31 12:43:49 +0200

    - Some fixes

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var escape = require('escape-html');
 
 app.use('/', express.static(__dirname + '/public'));
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+io.on('connection', function (socket) {
+    var id = socket.id;
+    socket.emit('user-accepted', {id: id});
 
-    socket.on('chat-message', function(messageData){
+    socket.on('chat-message', function (messageData) {
+        messageData['userId'] = id;
+        messageData['createdAt'] = Date.now();
+        messageData['msg'] = escape(messageData.msg);
+        messageData['nick'] = escape(messageData.nick);
         io.emit('recive-message', messageData);
     });
 });
 
 var port = process.env.PORT || 8080;
 
-http.listen(port, function(){
+http.listen(port, function () {
     console.log('listening on *:' + port);
 });
--- public/webpage-scripts.js
+++ public/webpage-scripts.js
@@ -1,23 +1,64 @@
 (function () {
     var socket = io();
 
-    $('#form').submit(function () {
+    var name = getName();
+    var userId = '';
+
+    var $messages = $('#messages');
+
+    $('#new-message').submit(function (e) {
+        e.preventDefault();
+
         var data = {
-            'nick': $('#user-name').val(),
-            'msg' : $('#message-content').val()
+            'nick': name,
+            'msg': $('#message-content').val()
         };
 
         socket.emit('chat-message', data);
-        $('#m').val('');
+        $('#message-content').val('');
+
         return false;
     });
 
     socket.on('recive-message', function (data) {
-        $('#messages').append(
-            '<li>' +
-            '   <div class="nick-name">data.nick</div>' +
-            '   <div class="incomming-message">data.msg</div>' +
-            '</li>'
-        );
+        var $message = $('<li/>');
+
+        if (data.userId === userId) {
+            $message.addClass('my-message');
+        }
+
+        var date = new Date(data.createdAt);
+
+        $('<div/>')
+            .addClass('nick-name')
+            .text(data.nick + ' at: ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds())
+            .appendTo($message);
+
+        $('<div/>')
+            .addClass('incomming-message')
+            .text(data.msg)
+            .appendTo($message);
+
+        $message.appendTo($messages);
     });
+
+    socket.on('user-accepted', function (data) {
+        userId = data.id;
+    });
+
+    function getName() {
+        if (localStorage.getItem('userName')) {
+            return localStorage.getItem('userName');
+        }
+
+        var name = prompt('Please enter your name.');
+
+        if (name === '' || name === null) {
+            return getName();
+        }
+
+        localStorage.setItem('userName', name);
+
+        return name;
+    }
 })();
